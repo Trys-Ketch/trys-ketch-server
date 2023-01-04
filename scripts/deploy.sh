b3ed54a2b950e5a9d7469abf10a42c26ec3d51b2
JAE YEONG KIM <97332044+code0613@users.noreply.github.com>
2023-01-04 20:16:35 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
