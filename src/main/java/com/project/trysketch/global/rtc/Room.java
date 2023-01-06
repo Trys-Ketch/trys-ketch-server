95916bc941a3dc5ba14334af6c24ccd917563ea3
eunsol-an <dev.eunsol@gmail.com>
2023-01-06 23:37:45 +0900

    [FEAT] WebRTC, WebSocket 관련 Signaling Server 구축
    - WebRtcConfig, WebSocketConfig 생성
    - SignalingHandler 러프하게 작성, 추후 수정 예정
    - 클라이언트와 테스트 필요

---
+++ src/main/java/com/project/trysketch/global/rtc/Room.java
@@ -0,0 +1,21 @@
+package com.project.trysketch.global.rtc;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 1. 기능   : WebRTC관련 Room domain
+// 2. 작성자 : 안은솔
+
+@Getter
+@AllArgsConstructor
+public class Room {
+    @NotNull
+    private final Long id;
+    // sockets by user names
+    private final Map<String, WebSocketSession> clients = new HashMap<>();
+}
