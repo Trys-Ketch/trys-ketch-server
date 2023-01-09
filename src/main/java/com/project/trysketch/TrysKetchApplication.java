53c49e7b9a7afc87ea2d580c3ce7a20c2474256c
beautifulseoul <1991scarlett@gmail.com>
2023-01-10 00:17:58 +0900

    [FIX] 잘못 삭제한 @EnableCaching 추가 (#29)

--- src/main/java/com/project/trysketch/TrysKetchApplication.java
+++ src/main/java/com/project/trysketch/TrysKetchApplication.java
@@ -2,11 +2,13 @@ package com.project.trysketch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication
 public class TrysKetchApplication {
 
