35d2ef7c0ecf610bf04cc9473981b07cae994654
eunsol-an <dev.eunsol@gmail.com>
2023-01-11 14:02:48 +0900

    [FEAT] long -> Long으로 변경, 사용하지 않는 import 삭제 (#38)

--- src/main/java/com/project/trysketch/global/rtc/Message.java
+++ src/main/java/com/project/trysketch/global/rtc/Message.java
@@ -18,7 +18,7 @@ public class Message {
     private String sender;    // 보내는 유저 UUID
     private String type;      // 메시지 타입
     private String receiver;  // 받는 사람
-    private long room;      // roomId
+    private Long room;      // roomId
     private Object candidate; // 상태
     private Object sdp;       // sdp 정보
     private Object allUsers;  // 해당 방에 본인을 제외한 전체 유저
--- src/main/java/com/project/trysketch/repository/GameRoomRepository.java
+++ src/main/java/com/project/trysketch/repository/GameRoomRepository.java
@@ -1,10 +1,7 @@
 package com.project.trysketch.repository;
 
 import com.project.trysketch.entity.GameRoom;
-import com.project.trysketch.entity.GameRoomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 // 1. 기능   : 게임 방 repository
 // 2. 작성자 : 김재영
