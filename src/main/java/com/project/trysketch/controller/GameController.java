a0d419c58ed5595e23281df7b5c0f3df9d83625b
code0613 <vhxmfltm3000@naver.com>
2023-01-12 15:06:19 +0900

    [FEAT] 게임 메인 로직 구현 (#26)

--- src/main/java/com/project/trysketch/controller/GameController.java
+++ src/main/java/com/project/trysketch/controller/GameController.java
@@ -20,9 +20,36 @@ public class GameController {
 
     //  게임시작
     @PostMapping("/game/{roomId}/start")
-    public ResponseEntity<DataMsgResponseDto> startGame(@PathVariable Long roomId
-                                                    , HttpServletRequest request){
+    public ResponseEntity<DataMsgResponseDto> startGame(@PathVariable Long roomId, HttpServletRequest request){
         log.info(">>> 게임이 시작되었습니다 - 게임 방 번호 : {},",roomId);
         return ResponseEntity.ok(gameService.startGame(roomId,request));
     }
+
+    // 제시어 줘
+    @PostMapping("/ingame/{keywordnum}/{round}")
+    public void keywordStatus(@PathVariable Long keywordnum, @PathVariable Long round){
+        gameService.
+    }
+
+
+//
+//    api(순번, 라운드, {라운드데이터})
+//1user : 제시어 api(1,1)
+//2user : 제시어 api(2,1)
+//3user : 제시어 api(3,1)
+//4user : 제시어 api(4,1)
+//5user : 제시어 api(5,1)
+
+
+                그림 테이블 : (user's nickname), roundId, keyword's Id, gameUd
+                제시어 테이블:                   roundId, keyword's Id, gameid
+
+            [ [제시어, 그림, 제시어(5번유저) ],
+            [제시어, 그림, 제시어(4번유저), 제시어, 그림, 제시어],
+            [제시어, 그림, 제시어(5번유저) ],
+            [제시어, 그림, 제시어(1번유저) ],
+            [제시어, 그림, 제시어(2번유저), 그림, 제시어 ]]
+
+
+
 }
