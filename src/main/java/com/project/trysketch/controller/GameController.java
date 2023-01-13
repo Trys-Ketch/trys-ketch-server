7f4ff13e7681728adcf0552a5802d35df0a02c2b
code0613 <vhxmfltm3000@naver.com>
2023-01-13 11:15:28 +0900

    [REFECTOR] 코드 리팩토링 코드 여백 수정, 서버로그 추가

--- src/main/java/com/project/trysketch/controller/GameController.java
+++ src/main/java/com/project/trysketch/controller/GameController.java
@@ -48,8 +48,7 @@ public class GameController {
 
     // 그림 제출하는 라운드 끝났을 때 for Test!
     @PostMapping("/test/finish/{roomId}/image")
-    public MsgResponseDto submitImage(@RequestParam int round,@RequestParam int keywordIndex, @PathVariable Long roomId
-            , @RequestParam String imagePath) {
+    public MsgResponseDto submitImage(@RequestParam int round,@RequestParam int keywordIndex, @PathVariable Long roomId, @RequestParam String imagePath){
         log.info(">>>>>> {} : 그림 라운드 끝",round);
         log.info(">>>>>> {} : 받을 제시어 순번",keywordIndex);
         log.info(">>>>>> {} : 게임 방 번호",roomId);
@@ -60,7 +59,7 @@ public class GameController {
 
     // 단어적는 라운드 시작  ← 이전 라운드의 그림 response 로 줘야함! (GepMapping)
     @GetMapping("/test/start/{roomId}/before/image")
-    public ImageResponseDto startKeywordRound(@RequestParam int round,@RequestParam int keywordIndex, @PathVariable Long roomId) {
+    public ImageResponseDto startKeywordRound(@RequestParam int round,@RequestParam int keywordIndex, @PathVariable Long roomId){
         log.info(">>>>>> {} : 라운드 시작",round);
         log.info(">>>>>> {} : 받을 제시어 순번",keywordIndex);
         log.info(">>>>>> {} : 게임 방 번호",roomId);
@@ -69,10 +68,10 @@ public class GameController {
 
     // 그림그리는 라운드 시작  ← 이전 라운드의 단어 response 로 줘야함! (GetMapping)
     @GetMapping("/test/start/{roomId}/before/keyword")
-    public KeywordResponseDto startImageRound(@RequestParam int round, @RequestParam int keywordIndex, @PathVariable Long roomId) {
+    public KeywordResponseDto startImageRound(@RequestParam int round, @RequestParam int keywordIndex, @PathVariable Long roomId){
         log.info(">>>>>>> {} : 이번 라운드", round);
-
-//        log.info(">>>>>>> {} : ");
+        log.info(">>>>>> {} : 받을 제시어 순번",keywordIndex);
+        log.info(">>>>>> {} : 게임 방 번호",roomId);
         return gameService.startImageRound(round, keywordIndex, roomId);
     }
 
