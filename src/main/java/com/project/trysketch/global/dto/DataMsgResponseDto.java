2bbad4d6d42b041c92dceb59f9247b4e7d022478
beautifulseoul <1991scarlett@gmail.com>
2023-01-17 23:47:16 +0900

    [REFACTOR] DataMsgResponseDto와 MsgResponseDto의 필드값 통일 (#68)

--- src/main/java/com/project/trysketch/global/dto/DataMsgResponseDto.java
+++ src/main/java/com/project/trysketch/global/dto/DataMsgResponseDto.java
@@ -3,7 +3,6 @@ package com.project.trysketch.global.dto;
 import com.project.trysketch.global.exception.StatusMsgCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 // 1. 기능   : 메시지와 데이터를 반환하는 dto
 // 2. 작성자 : 김재영
@@ -11,12 +10,12 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 public class DataMsgResponseDto {
     private int statusCode;
-    private String statusMsg;
+    private String message;
     private Object data;
 
     public DataMsgResponseDto(StatusMsgCode statusMsgCode, Object data) {
         this.statusCode = statusMsgCode.getHttpStatus().value();
-        this.statusMsg = statusMsgCode.getDetail();
+        this.message = statusMsgCode.getDetail();
         this.data = data;
     }
 }
