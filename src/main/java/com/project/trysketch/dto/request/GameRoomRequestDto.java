b024d098dd6dd9f7a1184ba2c7eae0dea8ac5050
beautifulseoul <1991scarlett@gmail.com>
2023-01-18 16:54:55 +0900

    [REFACTOR] 정규식 수정 (#73)

--- src/main/java/com/project/trysketch/dto/request/GameRoomRequestDto.java
+++ src/main/java/com/project/trysketch/dto/request/GameRoomRequestDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 // 1. 기능    : 게임 방 제목 입력 요소
 // 2. 작성자  : 김재영
@@ -11,6 +12,6 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 @NoArgsConstructor
 public class GameRoomRequestDto {
-    @Pattern(regexp = "^[a-zA-Z0-9가-힣`~!@#$%^&*()_=+|{};:,.<>/?]{2,25}$", message = "방제목은 2자이상, 25자이하이어야하며 한글, 대소문자, 숫자, 특수문자만 가능합니다.")
+    @Size(min=2, max= 25, message="방제목은 2자이상, 25자이하이어야합니다")
     private String title;
 }
