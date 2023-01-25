7c7acc7a4208961f2f72e33a8b77a7224fb6a1ae
code0613 <vhxmfltm3000@naver.com>
2023-01-25 22:36:05 +0900

    [FEAT] History, Achievement 관련 기능 구현 (#117) 로그인시 getTrophyOfVisit 메소드를 통해 방문횟수 확인해서 업적 만들기 기능 추가

--- src/main/java/com/project/trysketch/service/NaverService.java
+++ src/main/java/com/project/trysketch/service/NaverService.java
@@ -21,12 +21,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import java.util.UUID;
 
 // 1. 기능    : OAuth2.0 네이버 비즈니스 로직
@@ -54,18 +56,13 @@ public class NaverService {
     @Value("${spring.security.oauth2.client.provider.naver.token-uri}")
     String token_uri;
 
-
+    @Transactional
     public MsgResponseDto naverLogin(String code, String state,HttpServletResponse response) throws JsonProcessingException {
+        log.info(">>>>>>>>>>>>>>>>>> [NaverService] - naverLogin");
         String randomNickname = userService.RandomNick().getMessage();
-        log.info(">>>>>>>>>>>>>>>> [NaverService] - naverLogin");
-
-        log.info(">>>>>>>>>>>>>>>> code : {}",code);
-        log.info(">>>>>>>>>>>>>>>> state : {}",state);
-
 
         // 1. "인가 코드"로 "액세스 토큰" 요청
         String accessToken = getToken(code, state);      //포스트맨 테스트시 주석
-
         accessToken = accessToken.split(",")[0].split(":")[1];
 
         // 2. 토큰으로 Naver API 호출 : "액세스 토큰"으로 "Naver 사용자 정보" 가져오기
@@ -73,7 +70,12 @@ public class NaverService {
 
         User naverUser = registerNaverUserIfNeeded(naverUserInfo);
 
-        log.info(">>>>>>>>>>>>>>>> naverLogin - register 완료");
+        History history = naverUser.getHistory().updateVisits(1L);
+        historyRepository.saveAndFlush(history);
+
+        log.info(">>>>>>>>>>>>>>>>>> naver History의 주인 : {}",history.getUser().getId());
+        historyService.getTrophyOfVisit(naverUser);
+
         String createToken = jwtUtil.createToken(naverUser.getEmail(), naverUser.getNickname());
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, createToken);
 
@@ -131,30 +133,24 @@ public class NaverService {
         return new NaverRequestDto(id, email, randomNickname);
     }
 
-    private User registerNaverUserIfNeeded(NaverRequestDto naverUserInfo) {
+    @Transactional
+    public User registerNaverUserIfNeeded(NaverRequestDto naverUserInfo) {
         // DB 에 중복된 Naver Id 가 있는지 확인
         String naverId = naverUserInfo.getId();
-        User naverUser = userRepository.findByNaverId(naverId).orElseThrow(
-                () -> new CustomException(StatusMsgCode.USER_NOT_FOUND)
-        );
+        
+        User naverUser = userRepository.findByNaverId(naverId).orElse(null);
         if (naverUser == null) {
             // Naver 사용자 email 동일한 email 가진 회원이 있는지 확인
             String naverEmail = naverUserInfo.getEmail();
 
-            User sameEmailUser = userRepository.findByEmail(naverEmail).orElseThrow(
-                    () -> new CustomException(StatusMsgCode.USER_NOT_FOUND)
-            );
+            User sameEmailUser = userRepository.findByEmail(naverEmail).orElse(null);
 
             if (sameEmailUser != null) {
                 naverUser = sameEmailUser;
                 // 기존 회원정보에 NaverId 추가
                 naverUser = naverUser.naverIdUpdate(naverId);
-                userRepository.save(naverUser);
 
-                // 방문 횟수 1 증가!
-                History history = naverUser.getHistory().updateVisits(1L);
-
-                historyRepository.save(history);
+                naverUser = userRepository.saveAndFlush(naverUser);
             } else {
                 // history 생성부
                 History newHistory = historyService.createHistory();
@@ -169,18 +165,16 @@ public class NaverService {
                         .nickname(naverUserInfo.getNickname())
                         .email(email)
                         .imgUrl(userService.getRandomThumbImg().getMessage())
+                        .history(newHistory)
                         .build();
-                userRepository.save(naverUser);
-                newHistory.updateUser(naverUser);
-
-                // 방문 횟수 1 증가!
-                History history = naverUser.getHistory().updateVisits(1L);
-                historyRepository.save(history);
+                User user2 = userRepository.saveAndFlush(naverUser);
+                User newUser = userRepository.findById(user2.getId()).orElseThrow(
+                        () -> new CustomException(StatusMsgCode.USER_NOT_FOUND)
+                );
+                newHistory.updateUser(newUser);
             }
         }
         return naverUser;
     }
-
-
 }
 
