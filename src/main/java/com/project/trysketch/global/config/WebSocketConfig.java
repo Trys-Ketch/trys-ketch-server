835d514ddce0b3be54a3d39a63948088fb361c9b
eunsol-an <dev.eunsol@gmail.com>
2023-01-30 02:18:08 +0900

    [FIX] WebSocket STOMP 버퍼 사이즈 증축 변경 (#160)
    - 주석 삭제

--- src/main/java/com/project/trysketch/global/config/WebSocketConfig.java
+++ src/main/java/com/project/trysketch/global/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.project.trysketch.global.config;
 
-import com.project.trysketch.global.stomp.AgentWebSocketHandlerDecoratorFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
