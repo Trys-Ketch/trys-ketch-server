51f3e65a8a9c6f3067fb75a29256b3477f87420a
SHsus1122 <tjgurtn1122@naver.com>
2023-02-07 11:53:03 +0900

    [REFACTOR] 코드 리팩토링 (#187)

--- src/main/java/com/project/trysketch/entity/Noun.java
+++ src/main/java/com/project/trysketch/entity/Noun.java
@@ -1,28 +1,22 @@
 package com.project.trysketch.entity;
 
-import lombok.*;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 // 1. 기능   : 명사 구성요소
 // 2. 작성자 : 서혁수
-@Data
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Entity
+@Getter
+@NoArgsConstructor
 public class Noun {
 
     @Id
-    @Column(name = "num")
-    private int num;
+    private int num;            // 명사 번호
 
     @Column
-    private String noun;
+    private String noun;        // 명사
 
-    //==생성자==//
-    public Noun(int num, String noun) {
-        this.num = num;
-        this.noun = noun;
-    }
 }
--- src/main/java/com/project/trysketch/service/GuestService.java
+++ src/main/java/com/project/trysketch/service/GuestService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -18,7 +17,6 @@ import java.nio.charset.StandardCharsets;
 @Service
 @RequiredArgsConstructor
 public class GuestService {
-
     private final RedisTemplate<String, Object> redisTemplate;
     private final GuestRepository guestRepository;
 
@@ -32,11 +30,12 @@ public class GuestService {
         Long num = guestIncrement("guestCount");    // 자동값 증가 키값 지정 및 시작
         Long guestId = 10000L + num;                    // 10000 번 부터 시작해서 1씩 증가(첫번째 값 10001)
 
+        // 새로운 guest 객체에 필요한 정보를 담아서 생성 후 DB에 저장
         Guest guest = new Guest(guestId
                 , guestId.toString()
                 , requestDto.getNickname()
-                , requestDto.getImgUrl());              // 새로운 guest 객체에 필요한 정보를 담아서 생성
-        guestRepository.save(guest);                    // DB 에 저장
+                , requestDto.getImgUrl());
+        guestRepository.save(guest);
 
         // 쉼표로 구분되는 문자열 형태로 만들어 줌
         String guestStr = String.format("%d,%s,%s", guestId, guest.getNickname(), guest.getImgUrl());
@@ -46,9 +45,6 @@ public class GuestService {
 
         // 헤더에 헤더값 지정 및 바디 값 넣어주기
         response.addHeader("guest", encodeResult);
-
-        // 디코딩 확인용 코드입니다. 최종적으로는 삭제하면 됩니다.
-        System.out.println("디코딩 결과 : " + URLDecoder.decode(guestStr, StandardCharsets.UTF_8));
 
         return new MsgResponseDto(StatusMsgCode.LOG_IN);
     }
